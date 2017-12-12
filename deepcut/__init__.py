bd328245c0651b0f20612b1a6f3e7a0d6d766b6b
Jirat Pasuksmit <jirat@ampostech.com>
2017-12-12 11:19:34 +0700

    train file rename

--- deepcut/__init__.py
+++ deepcut/__init__.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 # encoding: utf-8
 from .deepcut import tokenize, DeepcutTokenizer
-from .train import generate_best_dataset, prepare_feature, train_model, evaluate
+from .train_deepcut import generate_best_dataset, prepare_feature, train_model, evaluate
