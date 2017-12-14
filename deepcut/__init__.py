41bd0728d1036b426e7b46bf1a44f67d809f922a
Jirat Pasuksmit <jirat@ampostech.com>
2017-12-14 11:39:03 +0700

    no message

--- deepcut/__init__.py
+++ deepcut/__init__.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
 # encoding: utf-8
 from .deepcut import tokenize, DeepcutTokenizer
+<<<<<<< Updated upstream
 from .train_deepcut import generate_best_dataset, prepare_feature, train_model, evaluate
+=======
+from .deepcut_train import generate_best_dataset, prepare_feature, train_model, evaluate
+>>>>>>> Stashed changes
