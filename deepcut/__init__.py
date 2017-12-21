44643abeee37cc279eeed4e720ba8909202dbb06
Jirat Pasuksmit <jirat@ampostech.com>
2017-12-21 14:38:41 +0700

    remove unused lines

--- deepcut/__init__.py
+++ deepcut/__init__.py
@@ -1,8 +1,4 @@
 #!/usr/bin/env python
 # encoding: utf-8
 from .deepcut import tokenize, DeepcutTokenizer
-<<<<<<< Updated upstream
 from .train_deepcut import generate_best_dataset, prepare_feature, train_model, evaluate
-=======
-from .deepcut_train import generate_best_dataset, prepare_feature, train_model, evaluate
->>>>>>> Stashed changes
